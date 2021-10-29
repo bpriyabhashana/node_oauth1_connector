10c8932656e1629cac6fba91a82153822d56bb29
bpriyabhashana <bpriyabhashana@gmail.com>
2021-10-29 13:54:25 +0530

    updated

--- backup.js
+++ backup.js
@@ -5,17 +5,17 @@ const oauth1a = require("oauth-1.0a");
 const axios = require("axios");
 
 const CONSUMERKEY =
-  "1167302c25e929dc0e3f7bb9506bcb50f09f7e7e6ddd316e166a9f74d940d8a5";
+  "add consumer key";
 const CONSUMERSECRET =
-  "1334c0149cb54d39451c35638512b3d9d4784ba614b4f42fd57dee716ebc8b20";
+  "add consumer secret";
 const TOKENKEY =
-  "695c3e49e35740538f4feab6d4427be1d61c6de47f48808ae7e031bead521d48";
+  "add token";
 const TOKENSECRET =
-  "69446859c5dabe6c27e0582fefad2fd011643e7fa37577d07a8d319939f7cc02";
+  "add secret";
 const REALM = "3883026_SB1";
 
 const request = {
-  url: "https://3883026-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=372&deploy=1",
+  url: "required url",
   method: "GET",
 };
 
